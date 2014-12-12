4f5261d61d166305791668226a91db1e5699e9ae
Alvin Liang <Alvin@Alvins-MBP.home>
2014-12-12 18:34:43 -0500

    Messed up this version

--- client.js
+++ client.js
@@ -1,14 +1,21 @@
-//var ws = new WebSocket("ws://localhost:3000");
-var ws = new WebSocket("ws://alvin.princesspeach.nyc:3000");
+var ws = new WebSocket("ws://localhost:3000");
+//var ws = new WebSocket("ws://alvin.princesspeach.nyc:3000");
 
-var userName = prompt("GIVE ME YOUR PHUCKING NAME").trim();
-var userColor = prompt("pick a color");
 
 var ul = document.createElement("ul");
 var body = document.querySelector("body");
 body.appendChild(ul);
 
 ws.addEventListener("open", function(evt){
+
+  var userName = prompt("GIVE ME YOUR PHUCKING NAME").trim();
+  if (userName > 0){
+  var jsName = JSON.stringify(userName);
+  ws.send(jsName);
+  console.log(jsName);
+  }
+  var userColor = prompt("pick a color");
+
   //addText('Current Status: Connected');
   var addText = function(msg){
     var newli = document.createElement("li");
@@ -44,3 +51,55 @@ input.addEventListener("keypress", function(evt){
   }
 });
 });
+
+/*var ws = new WebSocket("ws://localhost:3000");
+//var ws = new WebSocket("ws://alvin.princesspeach.nyc:3000");
+
+var userName = prompt("GIVE ME YOUR PHUCKING NAME").trim();
+//var jsName = JSON.stringify(userName);
+//ws.send(userName);
+//console.log(userName);
+var userColor = prompt("pick a color");
+
+var ul = document.createElement("ul");
+var body = document.querySelector("body");
+body.appendChild(ul);
+
+ws.addEventListener("open", function(evt){
+  //addText('Current Status: Connected');
+  var addText = function(msg){
+    debugger;
+    var newli = document.createElement("li");
+    var mssg = JSON.parse(msg);
+    var printMessage = mssg.name + ": " + mssg.newMessage;
+    console.log(mssg);
+    newli.style.color = mssg.color;
+    newli.innerHTML = printMessage;
+    var firstli = ul.firstChild;
+    ul.insertBefore(newli, firstli);
+  }
+
+ws.addEventListener("message", function(evt){
+  addText(evt.data);
+  });
+
+button.addEventListener("click", function(evt){
+  var inputStr = document.querySelector("#input");
+  var userMessage = {name: userName, newMessage: inputStr.value, color: userColor};
+  // user["strings"] = inputStr.toString().trim();
+  var info = JSON.stringify(userMessage);
+    ws.send(info);
+    input.value = " ";
+});
+
+input.addEventListener("keypress", function(evt){
+    if(evt.keyCode === 13){
+      var inputStr = document.querySelector("#input");
+      var userMessage = {name: userName, newMessage: inputStr.value, color: userColor};
+      var info = JSON.stringify(userMessage);
+    ws.send(info);
+    input.value = " ";
+  }
+});
+});
+*/
--- server.js
+++ server.js
@@ -2,26 +2,48 @@ var wss = require("ws").Server;
 var server = new wss({port:3000});
 var clients = [];
 var history = [];
+var userName = [];
 server.on("connection", function(ws){
-    if (history.length > 0) {
+    if (history.length > 0){
       history.forEach(function(msgz) {
         ws.send(msgz);
       })
     }
-
-  clients.push(ws);
-    ws.on("close", function(){
-      var x = clients.indexOf(ws);
-      clients.splice(x, 1);
+      clients.push(ws);
+      clients.forEach(function(client){
+      var robot = {name : "Robot", newMessage: client + " connected", color: "black"};
+      console.log(robot);
+      var broadcast = JSON.stringify(robot);
+      console.log(broadcast);
+      ws.send(broadcast);
+    })
+      ws.on("close", function(){
+        var x = clients.indexOf(ws);
+        clients.splice(x, 1);
+      });
+      ws.on("message", function(msg){
+        history.push(msg);
+        console.log(clients);
+        for(i=0; i<clients.length; i++){
+          clients[i].send(msg);
+        }
     });
-    ws.on("message", function(msg){
-      history.push(msg);
-      console.log(msg);
-      for(i=0; i<clients.length; i++){
-      clients[i].send(msg);
-    }
-  })
-});
+  });
+/*  clients.push(ws);
+  clients.forEach(function(person){
+    var pName = JSON.parse(person);
+    var robot = {name: "Robot", newMessage: pName + " connected", color: "black"};
+    console.log(robot);
+
+    person.send(broadcast);
+  });
+
+    var banned = function(userName){
+      var uname = userName.toLowerCase();
+      if((uname === "trey") || (uname=== "trey jackson")){
+        alert("Username is extremely offensive and banned");
+      }
+    }*/
 
 /*var WebSocketServer = require('ws').Server;
 var server = new WebSocketServer({port: 3000})
