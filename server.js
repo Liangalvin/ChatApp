a73de317fe776d053e73508cc535fa858ff35e5c
Alvin Liang <Alvin@Alvins-MacBook-Pro.local>
2014-12-13 13:12:57 -0500

    instead of [object object] it's now 'Robot: undefined connected'

--- server.js
+++ server.js
@@ -2,55 +2,32 @@ var wss = require("ws").Server;
 var server = new wss({port:3000});
 var clients = [];
 var history = [];
-var userName = [];
+var userNamez = [];
 server.on("connection", function(ws){
     if (history.length > 0){
       history.forEach(function(msgz) {
         ws.send(msgz);
       })
     }
+      userNamez.push(ws);
+      userNamez.forEach(function(jsNamed){
+        var robot = {name : "Robot", newMessage: jsNamed.name + " connected", color: "black"};
+        console.log(jsNamed);
+        //console.log(robot);
+        var broadcast = JSON.stringify(robot);
+      //  console.log(broadcast);
+        jsNamed.send(broadcast);
+      })
       clients.push(ws);
-      clients.forEach(function(client){
-      var robot = {name : "Robot", newMessage: client + " connected", color: "black"};
-      console.log(robot);
-      var broadcast = JSON.stringify(robot);
-      console.log(broadcast);
-      ws.send(broadcast);
-    })
       ws.on("close", function(){
         var x = clients.indexOf(ws);
         clients.splice(x, 1);
       });
       ws.on("message", function(msg){
         history.push(msg);
-        console.log(clients);
+        //console.log(clients);
         for(i=0; i<clients.length; i++){
           clients[i].send(msg);
         }
     });
   });
-/*  clients.push(ws);
-  clients.forEach(function(person){
-    var pName = JSON.parse(person);
-    var robot = {name: "Robot", newMessage: pName + " connected", color: "black"};
-    console.log(robot);
-
-    person.send(broadcast);
-  });
-
-    var banned = function(userName){
-      var uname = userName.toLowerCase();
-      if((uname === "trey") || (uname=== "trey jackson")){
-        alert("Username is extremely offensive and banned");
-      }
-    }*/
-
-/*var WebSocketServer = require('ws').Server;
-var server = new WebSocketServer({port: 3000})
-  server.on("connection", function(ws){
-    ws.on("message", function(msg){
-      var hash = JSON.parse(msg);
-      console.log(hash.name + ": " + hash.words);
-      ws.send(hash.name + ": " + hash.words);
-    })
-  })*/
